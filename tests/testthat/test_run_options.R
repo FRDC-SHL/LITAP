3d362b75ccbab3392812e43588ad13b196fee5bf
Stefanie LaZerte <steffi@steffi.ca>
2017-08-30 14:45:49 -0500

    Correct test data location

--- tests/testthat/test_run_options.R
+++ tests/testthat/test_run_options.R
@@ -5,13 +5,14 @@ cont_options <- c("directions", "watersheds", "watershed_area", "local", "pond",
                   "inverted", "iwatersheds", "iwatershed_area", "ilocal")
 
 test_that("Quiet is quiet and that runs continue and end as they should", {
-
   for(i in cont_options){
-    expect_warning(expect_message(complete_run(file = "./testELEV.dbf", nrow = 11, ncol = 11,
-                                               verbose = TRUE, continue = i, end = i)), NA)
+    expect_warning(expect_message(complete_run(file = "testELEV.dbf", nrow = 11, ncol = 11,
+                                               verbose = TRUE, continue = i, end = i,
+                                               report = FALSE)), NA)
 
-    expect_warning(expect_silent(complete_run(file = "./testELEV.dbf", nrow = 11, ncol = 11,
-                                              verbose = TRUE, quiet = TRUE, continue = i, end = i)), NA)
+    expect_warning(expect_silent(complete_run(file = "testELEV.dbf", nrow = 11, ncol = 11,
+                                              verbose = TRUE, quiet = TRUE, continue = i, end = i,
+                                              report = FALSE)), NA)
   }
 
   f <- list.files(".", full.names = TRUE, recursive = TRUE)
