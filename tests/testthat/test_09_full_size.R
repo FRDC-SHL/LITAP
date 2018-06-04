86f9a1e54e04b7adafc2ec85fd0677c89773cb1d
Stefanie LaZerte <steffi@steffi.ca>
2018-06-04 10:37:24 -0500

    Add long test

---
+++ tests/testthat/test_09_full_size.R
@@ -0,0 +1,16 @@
+context("Long test")
+
+test_that("Full example runs", {
+  skip_on_cran()
+  expect_message(complete_run(file = system.file("extdata", "testELEV.dbf",
+                                                 package = "LITAP"),
+                              folder_out = "./test_long",
+                              nrow = 150, ncol = 150))
+
+  t <- list.files("./test_long", recursive = TRUE, full.names = TRUE)
+  expect_length(t, 42)
+  file.remove(t)
+  file.remove("./test_long/backup/", "./test_long/dbf", "./test_long/final")
+  file.remove("./test_long")
+
+})
