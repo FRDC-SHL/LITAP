9501098c0e7593bbb0f935b02fc7c3e04fa28a7c
Steffi LaZerte <steffi@steffi.ca>
2019-06-11 09:49:16 -0500

    Finalize and tweak calc_relz

--- R/calc_relz.R
+++ R/calc_relz.R
@@ -1,25 +1,40 @@
 # Relief calculations type 3
 
-calc_relz <- function(db, idb, verbose = TRUE) {
+calc_relz <- function(db, idb, str_val = 10000, ridge_val = 10000, verbose = TRUE) {
 
-  temp1 <- calc_stream(db, verbose = verbose)
+  if(verbose) message("Calculating streams")
+  temp1 <- db %>%
+    dplyr::mutate(shedno = fill_shed) %>%
+    calc_stream(str_val = str_val, verbose = verbose)
+
   temp2 <- calc_pit(db)
-  temp3 <- calc_stream(idb) %>%
+
+  if(verbose) message("Calculating ridges")
+  temp3 <- calc_stream(idb, str_val = ridge_val, verbose = verbose) %>%
     dplyr::rename(cr_row = str_row, cr_col = str_col, cr_elev = str_elev,
-                  z2cr = z2st, n2cr = n2st)
+                  z2cr = z2st, n2cr = n2st) %>%
+    dplyr::mutate(cr_elev = max(db$elev, na.rm = TRUE) - cr_elev) #Convert back to orig elev
+
   temp4 <- calc_pit(idb) %>%
     dplyr::rename(peak_seqno = pit_seqno, peak_row = pit_row, peak_col = pit_col,
-                  peak_elev = pit_elev, z2peak = z2pit, n2peak = n2pit)
+                  peak_elev = pit_elev, z2peak = z2pit, n2peak = n2pit) %>%
+    dplyr::mutate(peak_elev = max(db$elev, na.rm = TRUE) - peak_elev)
 
   temp <- dplyr::left_join(temp1, temp2) %>%
     dplyr::left_join(temp3) %>%
-    dplyr::left_join(temp4)
+    dplyr::left_join(temp4) %>%
+    dplyr::left_join(dplyr::select(db, seqno, row, col, buffer, fill_shed))
 
+  if(verbose) message("Calculating relief")
   calc_relief(temp)
 }
 
 # To speed this up, consider breaking by watershed, or by flow-to-a-pit groups
 # and running in purrr::map or something similar
+calc_stream_sub <- function(db_shed, db, relz, str_val, verbose) {
+
+}
+
 
 calc_stream <- function(db, str_val = 10000, verbose = TRUE) {
   num_dn <- 0
@@ -28,7 +43,7 @@ calc_stream <- function(db, str_val = 10000, verbose = TRUE) {
   str_col <- 0
   str_elev <- 0
 
-  temp <- data.frame(seqno = db$seqno,
+  relz <- data.frame(seqno = db$seqno,
                      str_row = 0, str_col = 0, str_elev = 0,
                      z2st = 0, n2st = 0)
   db_temp <- dplyr::arrange(db, shedno, dplyr::desc(elev), upslope)
@@ -43,10 +58,10 @@ calc_stream <- function(db, str_val = 10000, verbose = TRUE) {
     track <- trace_flow2(i, db_temp)
 
     # Get first cell already visited
-    visited <- which(temp$z2st[track] > 0)[1]
+    visited <- which(relz$z2st[track] > 0)[1]
 
     # Get first channel cell
-    channel <- which(db_temp$upslope[track] > str_val)[1]
+    channel <- which(db_temp$upslope[track] >= str_val)[1]
 
     # Get final cell
     end <- track[min(c(visited, channel, length(track)), na.rm = TRUE)]
@@ -55,32 +70,36 @@ calc_stream <- function(db, str_val = 10000, verbose = TRUE) {
     # Get new track
     track <- track[1:num_dn]
 
+    # Update channel and visited
+    visited <- which(relz$z2st[track] > 0)[1]
+    channel <- which(db_temp$upslope[track] >= str_val)[1]
+
     # If we go to the end (i.e. no visited, no channels)
     # OR have a channel (and/or visited)
     if((is.na(visited) && is.na(channel)) || !is.na(channel)){
 
-      temp$str_row[track] <- db_temp$row[end]
-      temp$str_col[track] <- db_temp$col[end]
-      temp$str_elev[track] <- db_temp$elev[end]
+      relz$str_row[track] <- db_temp$row[end]
+      relz$str_col[track] <- db_temp$col[end]
+      relz$str_elev[track] <- db_temp$elev[end]
 
-      temp$z2st[track] <- db_temp$elev[track] - db_temp$elev[end]
-      temp$n2st[track] <- num_dn
+      relz$z2st[track] <- db_temp$elev[track] - db_temp$elev[end]
+      relz$n2st[track] <- seq(num_dn-1, by = -1, along.with = track)
 
     # If we go to the first cell already visited (i.e. cell is NOT channel cell)
     } else {
 
-      temp$str_row[track] <- temp$str_row[end]
-      temp$str_col[track] <- temp$str_col[end]
-      temp$str_elev[track] <- temp$str_elev[end]
+      relz$str_row[track] <- relz$str_row[end]
+      relz$str_col[track] <- relz$str_col[end]
+      relz$str_elev[track] <- relz$str_elev[end]
 
-      temp$z2st[track] <- db_temp$elev[track] - temp$str_elev[end]
-      temp$n2st[track] <- seq(num_dn + temp$n2st[end], by = -1, along.with = track)
+      relz$z2st[track] <- db_temp$elev[track] - relz$str_elev[end]
+      relz$n2st[track] <- seq(num_dn + relz$n2st[end] - 1, by = -1, along.with = track)
     }
   }
-  temp$n2st <- temp$n2st - 1
-  temp$z2st[temp$z2st < 0] <- 0
 
-  temp
+  relz$z2st[relz$z2st < 0] <- 0
+
+  relz
 }
 
 # use pond db
@@ -109,12 +128,11 @@ calc_pit <- function(db) {
 
 # calcrelief3
 calc_relief <- function(relz) {
-
   relz %>%
     dplyr::mutate(min_elev = min(elev, na.rm = TRUE),
                   max_elev = max(elev, na.rm = TRUE),
                   elev_range = max_elev - min_elev) %>%
-    dplyr::group_by(shedno) %>%
+    dplyr::group_by(fill_shed) %>%
     dplyr::mutate(max_elev_shed = max(elev, na.rm = TRUE)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(zpit2peak = z2pit + z2peak,
@@ -123,11 +141,10 @@ calc_relief <- function(relz) {
                   z2top = max_elev_shed - elev,
                   ztop2pit = max_elev_shed - pit_elev,
                   ncr2st = n2cr + n2st,
-                  pctz2top = 100 - round((z2top / ztop2pit) * 100),
-                  pctz2st = round((z2st / zcr2st) * 100),
-                  pctz2pit = round((z2pit / zpit2peak) * 100),
-                  pctN2st = round((n2st / ncr2st) * 100),
-                  pmin2max = round(((elev - min_elev) / elev_range) * 100))
-
+                  pctz2top = trunc(100 - (z2top / ztop2pit) * 100),
+                  pctz2st = trunc((z2st / zcr2st) * 100),
+                  pctz2pit = trunc((z2pit / zpit2peak) * 100),
+                  pctn2st = trunc((n2st / ncr2st) * 100),
+                  pmin2max = trunc(((elev - min_elev) / elev_range) * 100))
 }
 
