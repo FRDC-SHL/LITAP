8dcbae32193ca8012d3fa8f0ab73213f48deffb9
Steffi LaZerte <steffi@steffi.ca>
2024-09-26 12:29:39 -0500

    Check for correct facets

--- R/checks.R
+++ R/checks.R
@@ -1,12 +1,17 @@
 
-check_facets <- function(facet,
-                         nm = c("cst", "ups", "mid", "low", "dep"),
-                         fun = warning) {
+check_facets <- function(folder,
+                         nm = c("cst", "ups", "mid", "low", "dep")) {
 
-  if(!all(nm %in% names(facet))) {
-    fun("This step requires facets '",
-            paste0(nm, collapse = "', '"),
-            ". Use `facet_mapp()` with a `crule` file with the relevant facets",
+  c1 <- list_previous(folder, where = "facet", step = "fuzc", check_only = TRUE)
+  if(c1) facet <- get_previous(folder, where = "facet", step = "fuzc")
+  c2 <- file.exists(file.path(folder, "topographic_derivatives.xlsx"))
+
+  if(!c1 || !c2 || !all(nm %in% names(facet))) {
+    warning("Skipping Topographic summary...\n",
+            "  This step requires a facets output file `dem_fuzc` with facets '",
+            paste0(nm, collapse = "', '"), ",\n",
+            "  as well as a topographic_derivatives.xlsx file.\n",
+            "  Use `facet_mapper()` with a `crule` file defining the relevant facets and *no* `arule` file.",
             call. = FALSE)
     r <- FALSE
   } else r <- TRUE
@@ -18,3 +23,4 @@ check_folder <- function(folder, fun = warning) {
     fun("This folder doesn't exist: ", folder, call. = FALSE)
   }
 }
+
