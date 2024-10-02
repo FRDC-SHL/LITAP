db04326b83c74dce6ce4e6da71291270dc379437
Steffi LaZerte <steffi@steffi.ca>
2024-10-01 21:08:50 -0500

    Update docs

--- data-raw/formal_tests.R
+++ data-raw/formal_tests.R
@@ -6,73 +6,70 @@ library(LITAPReports)
 # AB provost site -------------------------------------------------------------
 # Need to give grid size
 
-f <- "../Runs - LITAP/11_Ab02PV"
-
 report_compare_flow(
   file = "../Runs - FlowMapR/Steffi_LandMapR_tests/11_Ab02PV/FlowMapR/021ELEV.DBF",
   nrow = 184, ncol = 187, grid = 5, max_area = 1, max_depth = 0.1,
-  out_folder = f)
+  out_folder = "../Runs - LITAP/11_Ab02PV")
 #pkgdown::build_article("reports/11_Ab02PV_compare_report_flow")
 
 report_compare_form(
-  folder = f,
+  folder = "../Runs - LITAP/11_Ab02PV",
   orig_loc = "../Runs - FlowMapR/Steffi_LandMapR_tests/11_Ab02PV/FlowMapR/",
   str_val = 200, ridge_val = 200)
 #pkgdown::build_article("reports/11_Ab02PV_compare_report_form")
 
 report_compare_facet(
-  folder = f,
-  arule = NULL, crule = system.file("extdata", "c7rule.dbf", package = "LITAP"),
+  folder = "../Runs - LITAP/11_Ab02PV",
+  arule = system.file("extdata", "arule.dbf", package = "LITAP"),
+  crule = system.file("extdata", "crule.dbf", package = "LITAP"),
   orig_loc = "../Runs - FlowMapR/Steffi_LandMapR_tests/11_Ab02PV/FlowMapR/")
 #pkgdown::build_article("reports/11_Ab02PV_compare_report_facet")
 
 report_compare_wepp(
-  folder = f,
+  folder = "../Runs - LITAP/11_Ab02PV",
   orig_loc = "../Runs - FlowMapR/Steffi_LandMapR_tests/11_Ab02PV/FlowMapR/",
   chan_length = 200, upslope_threshold = 300)
 
 # BR3 ------------------------------------------------------------------------
 # Should detect grid size of 1
-f <- "../Runs - LITAP/BR3"
 
 report_compare_flow(
   file = "../Runs - FlowMapR/BR3/31MELEV.DBF",
   nrow = 309, ncol = 181, max_area = 100, max_depth = 0.1,
-  out_folder = f)
-#pkgdown::build_article("reports/BR3_compare_report_flow")
+  out_folder = "../Runs - LITAP/BR3")
+pkgdown::build_article("reports/BR3_compare_report_flow")
 
 report_compare_form(
-  folder = f,
+  folder = "../Runs - LITAP/BR3",
   orig_loc = "../Runs - FlowMapR/BR3/",
   str_val = 300, ridge_val = 300)
-#pkgdown::build_article("reports/BR3_compare_report_form")
+pkgdown::build_article("reports/BR3_compare_report_form")
 
 report_compare_facet(
-  folder = f,
+  folder = "../Runs - LITAP/BR3",
   arule = NULL,
   crule = system.file("extdata", "c7rule.dbf", package = "LITAP"),
   orig_loc = "../Runs - FlowMapR/BR3/")
-#pkgdown::build_article("reports/BR3_compare_report_facet")
+pkgdown::build_article("reports/BR3_compare_report_facet")
 
 # Ehsan ---------------------------------------------------------------------
 # Need to give grid size
-f <- "../Runs - LITAP/Ehsan"
 
 report_compare_flow(
   file = "../Runs - FlowMapR/Ehsan/05ELEV.dbf", grid = 5,
   nrow = 50, ncol = 42, max_depth = 0.2, max_area = 10,
-  out_folder = f)
-#pkgdown::build_article("reports/Ehsan_compare_report_flow")
+  out_folder = "../Runs - LITAP/Ehsan")
+pkgdown::build_article("reports/Ehsan_compare_report_flow")
 
 report_compare_form(
-  folder = f,
+  folder = "../Runs - LITAP/Ehsan",
   orig_loc = "../Runs - FlowMapR/Ehsan/",
   str_val = 200, ridge_val = 200)
-#pkgdown::build_article("reports/Ehsan_compare_report_form")
+pkgdown::build_article("reports/Ehsan_compare_report_form")
 
 report_compare_facet(
-  folder = f,
+  folder = "../Runs - LITAP/Ehsan",
   arule = "../Runs - LITAP/Ehsan/LM3ARULE.dbf",
   crule = "../Runs - LITAP/Ehsan/LM3CRULE.dbf",
   orig_loc = "../Runs - FlowMapR/Ehsan/")
-#pkgdown::build_article("reports/Ehsan_compare_report_facet")
+pkgdown::build_article("reports/Ehsan_compare_report_facet")
